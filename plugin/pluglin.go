3aa648d2ac17d2556600a8feed20d983078c29e2
sedflix <sedflix@gmail.com>
2020-09-06 19:22:14 +0530

    added config root_path

--- plugin/pluglin.go
+++ plugin/pluglin.go
@@ -22,9 +22,21 @@ func init() {
 		Config: &Config{},
 		InitFn: func(ic *plugin.InitContext) (interface{}, error) {
 			ic.Meta.Platforms = append(ic.Meta.Platforms, platforms.DefaultSpec())
+
+			// get config
+			config, ok := ic.Config.(*Config)
+			if !ok {
+				return nil, errors.New("invalid aufs configuration")
+			}
+
+			// use default ic.Root as root path if config doesn't have a valid root path
+			root := ic.Root
+			if len(config.RootPath) != 0 {
+				root = config.RootPath
+			}
 			ic.Meta.Exports["root"] = ic.Root
 
-			snapshotter, err := aufs.New(ic.Root)
+			snapshotter, err := aufs.New(root)
 			if err != nil {
 				return nil, errors.Wrap(plugin.ErrSkipPlugin, err.Error())
 			}
