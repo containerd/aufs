a7aca260f2b47880a8c28ed983918a82dd860faf
sedflix <sedflix@gmail.com>
2020-09-06 19:36:12 +0530

    add root_path in config
    Signed-off-by: sedflix <sedflix@gmail.com>

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -24,15 +24,33 @@ import (
 )
 
 
+// Config represents configuration for the zfs plugin
+type Config struct {
+	// Root directory for the plugin
+	RootPath string `toml:"root_path"`
+}
+
 func init() {
 	plugin.Register(&plugin.Registration{
 		Type: plugin.SnapshotPlugin,
 		ID:   "aufs",
 		InitFn: func(ic *plugin.InitContext) (interface{}, error) {
 			ic.Meta.Platforms = append(ic.Meta.Platforms, platforms.DefaultSpec())
-			ic.Meta.Exports["root"] = ic.Root
 
-			snapshotter, err := aufs.New(ic.Root)
+			// get config
+			config, ok := ic.Config.(*Config)
+			if !ok {
+				return nil, errors.New("invalid aufs configuration")
+			}
+
+			// use default ic.Root as root path if config doesn't have a valid root path
+			root := ic.Root
+			if len(config.RootPath) != 0 {
+				root = config.RootPath
+			}
+			ic.Meta.Exports["root"] = root
+
+			snapshotter, err := aufs.New(root)
 			if err != nil {
 				return nil, errors.Wrap(plugin.ErrSkipPlugin, err.Error())
 			}
