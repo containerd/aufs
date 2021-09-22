9572f92aaa4bebfd94f322fb14f0a4708d4f3bef
zounengren <zounengren@cmss.chinamobile.com>
2021-09-22 11:14:17 +0800

    replace pkg/errors
    Signed-off-by: Zou Nengren <zouyee1989@gmail.com>

--- go.mod
+++ go.mod
@@ -1,10 +1,9 @@
 module github.com/containerd/aufs
 
-go 1.13
+go 1.16
 
 require (
-	github.com/containerd/containerd v1.5.4
-	github.com/containerd/continuity v0.1.0
-	github.com/pkg/errors v0.9.1
-	golang.org/x/sys v0.0.0-20210324051608-47abb6519492
+	github.com/containerd/containerd v1.5.5
+	github.com/containerd/continuity v0.2.0
+	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887
 )
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -17,10 +17,12 @@
 package plugin
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/containerd/aufs"
 	"github.com/containerd/containerd/platforms"
 	"github.com/containerd/containerd/plugin"
-	"github.com/pkg/errors"
 )
 
 // Config represents configuration for the zfs plugin
@@ -52,7 +54,7 @@ func init() {
 
 			snapshotter, err := aufs.New(root)
 			if err != nil {
-				return nil, errors.Wrap(plugin.ErrSkipPlugin, err.Error())
+				return nil, fmt.Errorf("%s: %w", err.Error(), plugin.ErrSkipPlugin)
 			}
 			return snapshotter, nil
 		},
